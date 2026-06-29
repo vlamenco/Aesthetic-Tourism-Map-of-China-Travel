5a3107b61fbae58b0ab8c33504d293d3dfaf314a
vlamenco <545792605@qq.com>
2026-06-29 17:36:31 +0800

    Update map-data.ts

--- data/map-data.ts
+++ data/map-data.ts
@@ -24,7 +24,7 @@ export const AESTHETIC_MAP_DATA: MapLocation[] = [
     cityName: { zh: '开封', en: 'Kaifeng' },
     items: [
       {type: 'image',
-        name: { zh: 'A1.万岁山大宋武侠城', en: 'A1.Wansui Mountain (The Song Dynasty Of Kungfu City 万岁山)' },
+        name: { zh: 'A1.万岁山大宋武侠城', en: 'A1.Wansui Mountain (The Song Dynasty Of Kungfu City)' },
         value: `${GITHUB_IMAGE_BASE_URL}/wansuishandatiehua.jpg`,
         description: {zh: `万岁山是全世界最棒的人造游乐场！没有之一！
         音乐岛有内蒙古的马头琴和呼麦表演，有动物园，仙侠幻境城，王婆说媒，二娘的秀，武侠岛，神州广场的打铁花把我深深感动，仿佛他们要为我摘下天上最亮的星星……
@@ -33,7 +33,7 @@ export const AESTHETIC_MAP_DATA: MapLocation[] = [
         
         《无题》 元・郝经
         万岁山来穷九州，汴堤犹有万人愁。中原自古多亡国，亡宋谁知是石头？`,
-    en: `Wansui Mountain is the best man-made amusement park in the world, bar none! On Music Island, there are performances of the Inner Mongolian horsehead fiddle and khoomei. The park also houses a zoo, a Xianxia Fantasy City, Aunt Wang's Matchmaking Show, Second Madam's Stage Show, and a Martial Arts Island……
+    en: `Wansui Mountain 万岁山 is the best man-made amusement park in the world, bar none! On Music Island, there are performances of the Inner Mongolian horsehead fiddle and khoomei. The park also houses a zoo, a Xianxia Fantasy City, Aunt Wang's Matchmaking Show, Second Madam's Stage Show, and a Martial Arts Island……
       Chinese intangible cultural heritage performances from all across the country can be enjoyed here!
       The molten iron fireworks show at Divine Land Square moved me deeply, as if they were about to pluck the brightest star from the sky just for me...
       All performers and NPCs are extremely dedicated and professional, sticking firmly to their posts even during sandstorms and heavy rain. 
@@ -42,17 +42,17 @@ export const AESTHETIC_MAP_DATA: MapLocation[] = [
       Historical Origin：Wansui Mountain drained wealth from all the nine provinces, By Bian’s dykes, ten thousand souls still grieve. Since ancient times, many kingdoms fell in the Central Plains, Who knew it was stones that brought the Song to its end? （万岁山来穷九州，汴堤犹有万人愁。中原自古多亡国，亡宋谁知是石头？）`}},
     
       {type: 'image',
-  name: { zh: 'A2.繁塔', en: 'A2.Po Tower 繁Pó塔' },
+  name: { zh: 'A2.繁塔', en: 'A2.Po Tower' },
   value: `${GITHUB_IMAGE_BASE_URL}/pota.jpg`,
   description: {zh: `“铁塔只搭繁塔腰”。so，繁塔才是重点（读Pó，但每次打字都要别扭地“读错”）。
       外观像玉玺，六角形，只有三层高（残缺仍旧很美），在开封古城外。这里最有心的是每个雕砖都是一个人的寄托，祈福，和纪念。
       上至皇族、达官贵人，下至平民百姓都可以与它连接，朴素的心愿在这108种，近7000尊 法相庄严的菩萨、罗汉、乐伎等形象中，众志成城。`,
-    en: `The Iron Pagoda only reaches the waist of the Po Tower. So the Po Tower is the real highlight. Shaped like an imperial jade seal, this hexagonal pagoda stands merely three stories tall. Even in its incomplete state, it remains breathtakingly beautiful, situated outside the ancient city wall. 
+    en: `The Iron Pagoda 繁Pó塔 only reaches the waist of the Po Tower. So the Po Tower is the real highlight. Shaped like an imperial jade seal, this hexagonal pagoda stands merely three stories tall. Even in its incomplete state, it remains breathtakingly beautiful, situated outside the ancient city wall. 
       Each carved brick carries people\'s aspirations, prayers and remembrances. From imperial families and high-ranking officials down to ordinary folk, everyone has a bond with this pagoda. 
       Simple wishes converge and take form amid nearly 7,000 solemn statues of Buddhas, arhats, musicians and dancers across 108 different depictions.`}},
       
       {type: 'image',
-  name: { zh: 'A3.铁塔', en: 'A3.Iron Pagoda 铁塔' },
+  name: { zh: 'A3.铁塔', en: 'A3.Iron Pagoda' },
   value: `${GITHUB_IMAGE_BASE_URL}/tieta.jpg`,
   description: {
     zh: `参拜“天下第一塔”！
@@ -61,23 +61,23 @@ export const AESTHETIC_MAP_DATA: MapLocation[] = [
         让人无语的是，开封和日本户田居然是友好城市？！
         （我没有大格局，这事我不同意！）`,
     en: `Paying homage to the 1️⃣"Number One Pagoda Under Heaven"! 
-    The Iron Pagoda was originally a wooden structure built in 982 to enshrine the Buddha's relics tributated by King Qian Hongchu of Wuyue. It was later burned down by lightning. Reconstructed in 1049 during the reign of Emperor Renzong of the Northern Song Dynasty, it was built with brown glazed bricks.
+    The Iron Pagoda 铁塔 was originally a wooden structure built in 982 to enshrine the Buddha's relics tributated by King Qian Hongchu of Wuyue. It was later burned down by lightning. Reconstructed in 1049 during the reign of Emperor Renzong of the Northern Song Dynasty, it was built with brown glazed bricks.
     On June 5, 1938, Japanese invaders captured Kaifeng and bombarded the Iron Pagoda continuously, leaving large holes in the 8th and 9th floors. In 1952, Mao Zedong visited Kaifeng. Seeing the pagoda still standing despite being covered with bullet and shell marks, he said, "This Iron Pagoda represents us Chinese people. The Japanese can never defeat us!"
     What is truly frustrating is that Kaifeng is actually a sister city of Toda (Japan)?!
 (I may not be broad-minded, but I strongly disagree with this!).`}},
      
        { type: 'image',
-  name: { zh: 'A4.延庆观', en: 'A4.Yanqing Taoist Temple 延庆观' },
+  name: { zh: 'A4.延庆观', en: 'A4.Yanqing Taoist Temple' },
   value: `${GITHUB_IMAGE_BASE_URL}/yanqingguan.jpg`,
   description: { zh: `开封唯一留存的元代古建筑群，国内唯一汉+蒙融合的道教建筑。
         下方上圆，中原 “天圆地方” 礼制 + 蒙古包穹顶造型结合。
        注意看 琉璃屋脊雕刻了 蒙古骑狮武士、西域胡人，汉人被骑头上了（无奈）。北方之神“玄武”在元朝被推至为最重要的神，能感受到铁骑的bangbangbang吗？`,
-    en: `The only surviving ancient architectural complex of the Yuan Dynasty in Kaifeng, and the sole Taoist architecture in China blending Han汉 and Mongolian蒙 styles.
+    en: `The only surviving ancient architectural complex of the Yuan Dynasty in Kaifeng 延庆观, and the sole Taoist architecture in China blending Han汉 and Mongolian蒙 styles.
     It features a square base and a circular upper section, integrating the Central Plains ritual concept of "heaven is round and earth is square" with the dome shape of Mongolian yurts.
     Take a close look: glazed roof ridges are carved with Mongolian warriors riding lions, and Han people being ridden upon (helpless sigh). Xuanwu玄武, the God of the North, was elevated to the most supreme deity during the Yuan Dynasty. Can you almost hear the thunderous galloping of iron cavalry?`}},
      
             { type: 'image',
-  name: { zh: 'A5.大相国寺', en: 'A5.Daxiangguo Temple 大相国寺' },
+  name: { zh: 'A5.大相国寺', en: 'A5.Daxiangguo Temple' },
   value: `${GITHUB_IMAGE_BASE_URL}/daxiangguosi.jpg`,
   description: { zh: ` ·景云元年（710），韦后毒杀唐中宗，李隆基联合太平公主发动唐隆政变，诛杀韦氏一党，拥立相王李旦第二次登基，即唐睿宗。
     次年延和元年（712），睿宗忆自己数十年 “相王” 藩王身份入继大统的经历，下诏将汴梁新建的建国寺改名大相国寺，并亲笔题写寺额。 
@@ -86,7 +86,7 @@ export const AESTHETIC_MAP_DATA: MapLocation[] = [
     ·八角琉璃殿 里有整株千年银杏雕刻的千手千眼观音像，非常震撼，香火鼎旺。`,
     en: `In 710 year, Empress Wei韦后 poisoned Emperor Tang Zhongzong唐中宗. Li Longji李隆基 and Princess Taiping太平公主, launched the Tanglong Coup, executed all followers of the Wei clan, and installed Li Dan李旦 on the throne for the second time as Emperor Tang Ruizong唐睿宗.
 
-The following year (712), recalling his decades-long experience ascending the throne as a regional prince相王, Emperor Ruizong issued an imperial edict to rename the newly built Jianguo Temple in Bianliang（Kaifeng） as Daxiangguo Temple and personally inscribed the temple plaque.
+The following year (712), recalling his decades-long experience ascending the throne as a regional prince相王, Emperor Ruizong issued an imperial edict to rename the newly built Jianguo Temple in Bianliang（Kaifeng） as Daxiangguo Temple 大相国寺and personally inscribed the temple plaque.
 
 Little Story：
 ·It is said that Lu Zhishen鲁智深 （All Men Are Brothers: Water Margin《水浒传》） became a monk here and took charge of the vegetable garden, where the well-known tale "Lu Zhishen Uproots a Willow Tree" took place.
@@ -96,14 +96,14 @@ Little Story：
 ·The Octagonal Glazed Hall houses a statue of the Thousand-Handed and Thousand-Eyed Guanyin carved from an entire ginkgo tree, which presents a stunning sight and draws an endless stream of worshippers.`}},
     
        { type: 'image',
-  name: { zh: 'A6.山陕甘会馆', en: 'A6.Shaanxi-Shanxi-Gansu Guild Hall 山陕甘会馆' },
+  name: { zh: 'A6.山陕甘会馆', en: 'A6.Shaanxi-Shanxi-Gansu Guild Hall' },
   value: `${GITHUB_IMAGE_BASE_URL}/shanshanganduilian.jpg`,
   description: { zh: `建于清乾隆三十一年（1766 年），原址是明代开国功臣徐达府邸，既是三省同乡商会议事场所，也供奉关公，主打 “忠义经商” 理念，号称北方第一会馆。
       看古建筑的 照壁，中间是蜘蛛🕷，表示吐丝编织细密大网，八条蛛足聚拢财源、兼顾四方客商。关公看了不知作何感想？
       砖雕、石雕、木雕是绝技。挺像广州陈家祠的，但群体较小，免费讲解得还不错。
       
       喜欢这个对联“ 你也挤，我也挤，此地几无立脚地；好且看，歹且看，大家都有下场时。”`,
-    en: `Constructed in the Qianlong reign of the Qing Dynasty (1766), the site was originally the mansion of Xu Da, a meritorious founding minister of the Ming Dynasty. 
+    en: `Constructed in the Qianlong reign of the Qing Dynasty (1766), the site 山陕甘会馆 was originally the mansion of Xu Da, a meritorious founding minister of the Ming Dynasty. 
      It served as a venue for merchants from three provinces to discuss business, enshrined Guan Gong关公, upheld the philosophy of "conducting business with loyalty and righteousness", and was reputed as the top guild hall in northern China.
     Examine the screen wall of this ancient building; a spider🕷 sits at its center, symbolizing spinning an intricate vast web. The spider’s eight legs stand for gathering wealth and accommodating merchants from all directions.
     Businesspeople exist to seek gains, so we shouldn’t harp too much on Guan Gong’s ethics of loyalty and integrity.
@@ -114,37 +114,37 @@ Adore the couplet: "Crowded you are, crowded I am; scarce space is left for anyo
 Watch the good, watch the bad; every soul shall meet their final end."`}},
     
     {type: 'image',
-        name: { zh: 'A7.夜市小摊', en: 'A7.Night market stall 夜市' },
+        name: { zh: 'A7.夜市小摊', en: 'A7.Night market stall' },
         value: `${GITHUB_IMAGE_BASE_URL}/kaifengyeshixiaochi.png`,
         description: {  zh: `鼓楼 / 西司 / 老河大 夜市，我都经过了，本地人不推荐，就像大部分出名的古镇，没太多特色，年轻人打卡凑热闹还行，老外估计比较喜欢。`,
-    en: `I have passed by the night markets at Gulou, Xisi and Old Henan University. Foreigners can come over to experience the lively alleyways and unique late-night snack culture. 
+    en: `I have passed by the night markets at Gulou鼓楼, Xisi 西司and Old Henan University老河大 夜市. Foreigners can come over to experience the lively alleyways and unique late-night snack culture. 
     The Drum Tower Night Market (Gulou) has the longest history, breaking the boundaries between lanes and markets, with shops everywhere in a densely packed commercial model.`}},
    
     {type: 'image',
-        name: { zh: 'A8.开封府', en: 'A8.Kaifeng Prefecture 开封府' },
+        name: { zh: 'A8.开封府', en: 'A8.Kaifeng Prefecture' },
         value: `${GITHUB_IMAGE_BASE_URL}/kaifengfu.jpg`,
         description: {  zh: `一个新建的景点，没有玩的吃的，纯纯n:1仿造，在清心楼登高望远包公湖时，才觉得这票价值3折.`, 
-           en: `A newly built scenic spot with no entertainment facilities or food stalls; The area is an 1:5 replica of Song dynasty. 
+           en: `A newly built scenic spot with no entertainment facilities or food stalls; The area 开封府 is an 1:5 replica of Song dynasty. 
              Only when climbing up the Pure Heart Tower to overlook Baogong Lake did I feel the ticket price is worth a 70% discount off the original price.`},},
   
       {type: 'image',
-        name: { zh: 'B1.邢家锅贴', en: 'B1.Xing Guokui (Crispy Flatbread 邢家锅贴)' },
+        name: { zh: 'B1.邢家锅贴', en: 'B1.Xing Guokui (Crispy Flatbread)' },
         value: `${GITHUB_IMAGE_BASE_URL}/guotie.jpg`,
         description: { zh: '味道清新，造型像拿破仑的帽子。',
-          en: `Fresh taste and shaped like Napoleon's hat.
+          en: ` 邢家锅贴 Fresh taste and shaped like Napoleon's hat.
 "Impossible n'est pas français."`} },
     
      {type: 'image',
-        name: { zh: 'B2.黄河金鲤鱼焙面', en: 'B2.Yellow River Golden Carp 黄河金鲤鱼焙面' },
+        name: { zh: 'B2.黄河金鲤鱼焙面', en: 'B2.Yellow River Golden Carp' },
         value: `${GITHUB_IMAGE_BASE_URL}/huanghejinliyu.jpg`,
         description: {  zh: `在兴中楼清真饭店吃的，金鳞赤尾（专属激光刻字），很像松子鱼酸甜口，细挂面炸成棉被轻轻地盖上。让我长见识了！`,
-    en: `I had the carp at Xingzhonglou Halal Restaurant. It has golden scales and a red tail with exclusive laser engraving. 
+    en: `I had the carp 黄河金鲤鱼焙面 at Xingzhonglou Halal Restaurant. It has golden scales and a red tail with exclusive laser engraving. 
     tastes like sweet and sour pine nut fish.
 Thin noodles are fried fluffy and covered gently on top. 
 It broadened my horizons!`}},
 
        { type: 'image',
-  name: { zh: 'C1.包拯', en: 'C1.Bao Zheng 包公' },
+  name: { zh: 'C1.包拯', en: 'C1.Bao Zheng' },
   value: `${GITHUB_IMAGE_BASE_URL}/baozheng.jpg`,
   description: { zh: `宋代科举分 “解试【举人】、省试【省元】、殿试{进士:【一甲：状元、榜眼、探花，赐及第;
 二甲：赐出身;
@@ -154,18 +154,18 @@ It broadened my horizons!`}},
   （PS:不要来包公祠！会唱那首歌就行了！全是后建的，不值得）`,
     en: `The imperial examinations in the Song Dynasty were conducted in successive stages: the Provincial Preliminary Examination (for Juren candidates), the Metropolitan Examination (for Metropolitan Top Scholars), and the Palace Examination (for Jinshi scholars).
 Jinshi scholars were ranked into three tiers: the First Class, consisting of Zhuangyuan, Bangyan, Tanhua, who were granted the title of "Passed with Distinction"; the Second Class, granted the title of "Graduate"; and the Third Class, granted the title of "Associate Graduate".
-Bao Zheng passed the Jinshi examination at the age of 28, an achievement for someone so young to succeed in the imperial exams. However, he resigned from his official post to return home and care for his elderly parents.
+Bao Zheng 包公 passed the Jinshi examination at the age of 28, an achievement for someone so young to succeed in the imperial exams. However, he resigned from his official post to return home and care for his elderly parents.
 He devoted a decade to this filial duty and only resumed his official career after his parents passed away and he completed the traditional mourning period.
 For those of us who feel like we have "graduated" from the workplace at 35, hold on, never give up!
 The hopeful light will come.
 (No need to visit the Lord Bao Memorial Temple. All the buildings were reconstructed in recent years.)`}},
 
     {type: 'text',
-        name: { zh: 'D1.汴绣', en: 'D1.Bian Embroidery 汴绣' },
+        name: { zh: 'D1.汴绣', en: 'D1.Bian Embroidery' },
         value: { zh: `汴绣，也称'宋绣'，为中国十大名绣之一。《东京梦华录》记载，其专为赵宋王公与达官贵人绣制官服及装饰品，也被誉为“官绣”。
           1982年总结出新老针法共36种，可选书签、团扇、摆件等做纪念品，是"能揣走的中原美学"，可到宋都御街，或各大文创店购买。
           不过我更喜欢广绣，色彩再靓丽点~`,
-          en: `Also known as Song Embroidery, ranks among China’s Top Ten Famous Embroideries.
+          en: ` 汴绣 Also known as Song Embroidery, ranks among China’s Top Ten Famous Embroideries.
     According to *Dongjing Meng Hua Lu*, it was specially used to make official robes and decorative articles for nobles and high-ranking officials of the Song Dynasty, hence earning it the reputation of "Official Embroidery".
     In 1982, a total of 36 traditional and newly developed stitching techniques were sorted out. 
       You can choose bookmarks (~70￥), round silk fans, ornaments and other items as souvenirs. 
@@ -173,10 +173,10 @@ The hopeful light will come.
        links: [{text: 'Threading a path through time', url: 'https://www.chinadailyhk.com/hk/article/317111'}]},      
 
     {type: 'text',
-        name: { zh: 'D2.朱仙镇木版年画', en: 'D2.Zhuxian Town Woodblock New Year Paintings 朱仙镇木版年画' },
+        name: { zh: 'D2.朱仙镇木版年画', en: 'D2.Zhuxian Town Woodblock New Year Paintings' },
         value: { zh: `据孟元老《东京梦华录》记载，当时“市井皆印卖门神、钟馗、桃符及财神”等，其风格鲜明，以矿物、植物手工磨制颜料，多用青、黄、红三原色，色彩明快鲜艳，有乡土味。`,
-          en: `As recorded in *Dongjing Meng Hua Lu* by Meng Yuanyou, "door gods, Zhong Kui, peachwood charms and God of Wealth prints were all sold in markets". 
-          These prints featured a distinctive style, with pigments hand-ground from minerals and plants. Predominantly using the three primary colors of cyan, yellow and red, they boast bright, vivid hues brimming with rustic charm.
+          en: `As recorded in Dongjing Meng Hua Lu by Meng Yuanyou, "door gods, Zhong Kui, peachwood charms and God of Wealth prints were all sold in markets". 
+          These 朱仙镇木版年画 prints featured a distinctive style, with pigments hand-ground from minerals and plants. Predominantly using the three primary colors of cyan, yellow and red, they boast bright, vivid hues brimming with rustic charm.
           You can buy a Refrigerator magnets or decorative paintings carry an intangible cultural heritage with a history of over 1000 years.`},
        links: [{text: '木版年画', url: 'https://www.bilibili.com/video/BV1xB421r7uT/?vd_source=2ce4bbe44dc7454a15293090b00b4edc'}]},   
 
