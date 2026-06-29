f37d8197751acfdbd362e1c8d1a8a1b8bf325468
vlamenco <545792605@qq.com>
2026-06-29 17:17:10 +0800

    Update map-data.ts

--- data/map-data.ts
+++ data/map-data.ts
@@ -14,7 +14,7 @@ export interface MapLocation {
   items: MapItem[]
 }
 
-const GITHUB_IMAGE_BASE_URL = 'https://raw.githubusercontent.com/vlamenco/travel-img/main/images/';
+const GITHUB_IMAGE_BASE_URL = 'https://raw.githubusercontent.com/vlamenco/travel-img/main/images';
 /* https://raw.githubusercontent.com/vlamenco/Aesthetic-Tourism-Map-of-China-Travel/main/images/  */  
 
 export const AESTHETIC_MAP_DATA: MapLocation[] = [
